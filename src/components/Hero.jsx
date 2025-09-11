99809f89a1944d2cef9763b2a4a2abd0a818bff9
Adit <aditss2327@gmail.com>
2025-09-11 18:35:53 +0530

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -197,9 +197,6 @@ const Hero = ({ language, isDarkMode }) => {
       {/* Scroll Indicator */}
       <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-white/70 animate-bounce">
         <div className="flex flex-col items-center">
-          <span className="text-sm mb-2">
-            {language === 'en' ? 'Scroll to explore' : 'താഴേക്ക് സ്ക്രോൾ ചെയ്യൂ'}
-          </span>
           <div className="w-6 h-10 border-2 border-white/50 rounded-full flex justify-center">
             <div className="w-1 h-3 bg-white/70 rounded-full mt-2 animate-pulse"></div>
           </div>
